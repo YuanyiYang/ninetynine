52d4a8f3dfb39c9bfacc484643fc00d81d97c983
Yuanyi Yang <yuanyi.yang.8848@gmail.com>
2014-04-29 13:52:12 -0400

    i18n with cache file

---
+++ src/edu/nyu/smg/ninetyNine/graphics/GameConstants.java
@@ -0,0 +1,14 @@
+package edu.nyu.smg.ninetyNine.graphics;
+
+import com.google.gwt.i18n.client.Constants;
+
+public interface GameConstants extends Constants{
+
+	String clockwise();
+	
+	String antiClockwise();
+	
+	String submitButton();
+	
+	String subMoveButton();
+}
---
+++ src/edu/nyu/smg/ninetyNine/graphics/GameMessages.java
@@ -0,0 +1,7 @@
+package edu.nyu.smg.ninetyNine.graphics;
+
+import com.google.gwt.i18n.client.Messages;
+
+public interface GameMessages extends Messages{
+
+}
